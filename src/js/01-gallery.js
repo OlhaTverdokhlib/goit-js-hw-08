36d031c2be1afd3e21a72fef76ea8ee8d171b33a
Olha Tverdokhlib <balabolya@gmail.com>
2023-04-10 17:41:05 +0200

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const picturesContainer = document.querySelector('.gallery');
+const picturesMarkup = createPicturesMarkup(galleryItems);
+
+picturesContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+function createPicturesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+        <a class="gallery__link" href="${original}" onclick="return false;">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li> 
+`;
+    })
+    .join('');
+}
+
+const gallerySimple = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
